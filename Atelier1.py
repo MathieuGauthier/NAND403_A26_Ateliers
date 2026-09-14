039b5d623e7d95d8947713e83a1ef19a6031d4c3
Mathieu Gauthier <mgauthie54@etu.uqac.ca>
2026-09-14 19:00:36 -0400

    Test

--- Atelier1.py
+++ Atelier1.py
@@ -1,6 +1,6 @@
 Name = input("What is your name ? :")
 Town = input("Where are you from ? :")
 
-print("\n___Archive de la ville___\n")
+print("\n___Archive de la ville____\n")
 print(f"Name : {Name}")
 print(f"Town : {Town}")
--- hello_world.py
+++
@@ -1,6 +0,0 @@
-Name = input("What is your name ? :")
-Town = input("Where are you from ? :")
-
-print("\n___Archive de la ville___\n")
-print(f"Name : {Name}")
-print(f"Town : {Town}")
